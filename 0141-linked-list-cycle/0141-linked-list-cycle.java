b22eaac3a2da0704b4c678b6f1b9a88375c9d859
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-01-14 00:56:49 -0800

    Time: 5 ms (9.08%), Space: 44.5 MB (17.94%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> set=new HashSet<>();
+        while(head!=null){
+            if(set.contains(head)){
+                return true;
+            }
+            set.add(head);
+            head=head.next;
+        }
+        return false;
+    }
+}
+// T.C -> O(n)
+// S.C -> O(1)
+// Insert the nodes in the HashSet and whenever a node is encountered that is already present in the HashSet then return true.
