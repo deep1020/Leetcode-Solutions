647204d881fd779ce31457b330fd2697b19bc6f7
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-04-05 23:19:32 -0700

    Time: 32 ms (17.98%), Space: 45.2 MB (70.43%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isAlphabetic(ch)){ // Character.isLowerCase also works here
+                continue;
+            }
+            if(ch=='('){
+                st.push(i);
+            }
+            else{
+                if(!st.isEmpty() && s.charAt(st.peek())=='('){
+                    st.pop();
+                }
+                else{
+                    st.push(i);
+                }
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        HashSet<Integer> set=new HashSet<>(st);
+        for(int i=0;i<s.length();i++){
+            if(!set.contains(i)){
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
+// T.C -> O(n)
+// S.C -> O(n)
