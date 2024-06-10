372d5f008f86913092baca740e9edc4a4e761724
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-06-10 00:39:13 -0700

    Time: 0 ms (100%), Space: 41 MB (90.77%) - LeetHub

--- 1137-height-checker/1137-height-checker.java
+++ 1137-height-checker/1137-height-checker.java
@@ -1,15 +1,33 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int copy[]=Arrays.copyOf(heights,heights.length);
-        Arrays.sort(copy);
-        int count=0;
-        for(int i=0;i<heights.length;i++){
-            if(heights[i]!=copy[i]){
-                count++;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
+        for(int height:heights){
+            if(height<min){
+                min=height;
+            }
+            if(height>max){
+                max=height;
             }
         }
-        return count;
+        int count[]=new int[max-min+1];
+        for(int height:heights){
+            count[height-min]++;
+        }
+        int result=0;
+        int currHeight=min;
+        for(int i=0;i<heights.length;i++){
+            while(count[currHeight-min]==0){
+                currHeight++;
+            }
+            if(heights[i]!=currHeight){
+                result++;
+            }
+            count[currHeight-min]--;
+        }
+        return result;
     }
 }
-// T.C -> O(nlogn)
-// S.C -> O(n)
+// T.C -> O(n) n= number of elements in the heights array
+// S.C -> O(m) m= max element of heights, size of count array (max-min+1)
+// Count Sort Approach
