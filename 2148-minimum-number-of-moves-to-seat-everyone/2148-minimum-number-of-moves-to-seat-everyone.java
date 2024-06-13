b8a42642f4199d8456035917dac2826dc01ec2e2
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-06-12 21:40:33 -0700

    Time: 47 ms (5.22%), Space: 44.4 MB (14.32%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.java
@@ -1,13 +1,19 @@
 class Solution {
     public int minMovesToSeat(int[] seats, int[] students) {
-        int moves=0;
-        Arrays.sort(seats);
-        Arrays.sort(students);
+        int pos[]=new int[101];
         for(int i=0;i<seats.length;i++){
-            moves+=Math.abs(seats[i]-students[i]);
+            pos[seats[i]]++;
+            pos[students[i]]--;
+        }
+        int moves=0;
+        int current=0;
+        for(int i:pos){
+            moves+=Math.abs(current);
+            System.out.println(moves);
+            current+=i;
         }
         return moves;
     }
 }
-// T.C -> O(nlogn)
-// S.C -> O(1)
+// T.C -> O(n+m) Iterate through both seats and students takes O(2n) and then iterate through pos array to calculate number of moves needed to seat students take O(m)
+// S.C -> O(m) pos array takes space O(m)
