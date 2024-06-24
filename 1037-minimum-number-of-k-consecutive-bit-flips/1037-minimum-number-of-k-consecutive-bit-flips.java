d6a09e1d0b3d3c3ed3d78fb13b5e9c37893d1cea
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-06-24 14:56:14 -0700

    Time: 6 ms (65.63%), Space: 56.1 MB (68.75%) - LeetHub

--- 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
+++ 1037-minimum-number-of-k-consecutive-bit-flips/1037-minimum-number-of-k-consecutive-bit-flips.java
@@ -19,5 +19,18 @@ class Solution {
         return ans;
     }
 }
-// T.C -> O(n)
-// S.C -> O(n)
+// T.C -> O(n) loop iterates through each element of the nums array exactly once
+// S.C -> O(n) extra array flipped takes O(n) space. Variables like ans and count use O(1) space
+
+// ans: keeps track of the number of flips performed
+// count: track the number of flips within the current window
+// flipped: mark the positions where a flip has been performed
+// for loop iterates through each element of the nums array
+// For each element, the code checks if the current position is beyond the k-size window. If so, it adjusts the count by subtracting the flip status (flipped[i-k]) that is no longer in the window.
+// condition 1: nums[i] == 1 && count % 2 == 1
+// If the current element is 1 and the number of flips is odd, the actual value of the element (after applying the flips) is 0. This is because flipping an element an odd number of times inverts its value (1 becomes 0 and 0 becomes 1). Therefore, we need to flip it again to make it 1.
+// condition 2: nums[i] == 0 && count % 2 == 0 -> similar to condition 1
+// After both condition passed, code checks if flipping the next k bits is possible (i + k > nums.length)
+// If not, it returns -1 indicating it is impossible to achieve the goal
+// Otherwise, it increments ans and count, and marks the current position in the flipped array to indicate a flip has been performed
+// final value of ans, which represents the total number of flips required, is returned
