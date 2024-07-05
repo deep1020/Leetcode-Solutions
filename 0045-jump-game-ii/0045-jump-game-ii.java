a70abaeac64a3cc61e059f82231c1ab0a3dfb4ff
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-07-05 13:04:07 -0700

    Time: 1 ms (97.14%), Space: 44.7 MB (87.59%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length==1){
+            return 0;
+        }
+        int start=0,end=0;
+        int farthest=0;
+        int jump=0;
+        for(int i=0;i<nums.length-1;i++){
+            farthest=Math.max(farthest,i+nums[i]);
+            if(i==end){
+                end=farthest;
+                jump++;
+            }
+        }
+        return jump;
+    }
+}
+// T.C -> O(n)
+// S.C -> O(1)
+
+// Greedy Approach:
+
+// 1) Initialize all variables to 0
+// start: Tracks the beginning of the current range of indices that can be reached with the current number of jumps
+// end: Tracks the farthest point that can be reached with the current number of jumps
+// farthest: Tracks the farthest point that can be reached with the next jump
+// jump: Counts the number of jumps made
+// 2) Loop through each element in the array (except the last one)
+// For each index i, calculate the farthest point that can be reached with a jump from index i using farthest = Math.max(farthest, i + nums[i])
+// If i reaches the end of the current jump range (i == end):
+// Update end to farthest, the farthest point reachable with the next jump
+// Increment the jump counter by 1
+// Continue this process until the loop ends, which means all indices up to the second last index have been processed.
+// 3) Return total number of jumps needed to reach the last index
