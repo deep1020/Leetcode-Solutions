293583425ddcbae029c96c74c8986611a2b21092
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-07-05 18:22:55 -0700

    Time: 0 ms (100%), Space: 57.3 MB (55.04%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,11 +1,11 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         k=k%nums.length;
-        if(k>0){
+        // if(k>0){
             reverse(nums,0,nums.length-1);
             reverse(nums,0,k-1);
             reverse(nums,k,nums.length-1);
-        }     
+        // }     
     }
     public void reverse(int nums[],int left, int right){
         while(left<right){
