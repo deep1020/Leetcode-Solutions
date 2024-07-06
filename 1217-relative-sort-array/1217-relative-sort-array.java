73aabb5dd9de6db962ddfe145e5c3e4c3506b2bd
Deep Patel <114463998+deep1020@users.noreply.github.com>
2024-07-05 17:51:15 -0700

    Time: 0 ms (100%), Space: 41.8 MB (80.89%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.java
+++ 1217-relative-sort-array/1217-relative-sort-array.java
@@ -1,21 +1,33 @@
 class Solution {
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
-        // Count the frequency of each element in arr1
-        int count[]=new int[1001];
+        // Find maximum element in the given array
+        // int count[]=new int[1001];
+        // for(int num:arr1){
+        //     count[num]++;
+        // }
+        int maxLen=Integer.MIN_VALUE;
+        for(int i=0;i<arr1.length;i++){
+            maxLen=Math.max(maxLen,arr1[i]);
+        }
+        // Create a count array to store the frequency of each element of arr1
+        int count[]=new int[maxLen+1];
         for(int num:arr1){
             count[num]++;
         }
         // Sort arr1 based on arr2's order
         int index=0;
         for(int num:arr2){
-            while(count[num]-- > 0){
+            while(count[num]>0){
                 arr1[index++]=num;
+                count[num]--;
             }
         }
-        // Append the remaining elements in arr1
+        // Append the remaining elements in arr1 
+        // Example 1: 7 and 19 are not present in arr2 so include those in arr1 at the end
         for(int i=0;i<count.length;i++){
-            while(count[i]-- > 0){
+            while(count[i]>0){
                 arr1[index++]=i;
+                count[i]--;
             }
         }
         return arr1;
